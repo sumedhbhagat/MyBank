9fb8991007bbfeb4e4a28ec23848a6b449c90e54
Suhas Pachore <Suhas_Pachore@edgeverve.com>
2018-03-09 22:07:42 +0530

    added remote method /Customers/dicover

---
+++ common/models/customer.js
@@ -0,0 +1,32 @@
+module.exports = function(Customer) {
+    Customer.discover = function(identity, name, dob, cb){
+        Customer.find(
+            {'where': 
+                {'and':[
+                    {'name': name},
+                    {'dob':dob}
+                ]}
+            },
+            function(err, customersResult){
+                console.log(customersResult);
+                if(customersResult.length == 1){
+                    custId = customersResult[0].custId;
+                }
+                else {
+                    custId = "";
+                }
+
+                cb(null, custId);
+        });
+        
+    };
+
+    Customer.remoteMethod('discover', {
+        accepts: [
+            {arg: 'identity', type: 'string'},
+            {arg: 'name', type: 'string'},
+            {arg: 'dob', type: 'date'}
+        ],
+        returns: {arg: 'customerId', type: 'string'}
+    });
+};
